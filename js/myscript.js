6ce84be8683cb8800c4bb0c3ac8d126216616803
G-Lumia <giorgiolumia@gmail.com>
2023-03-13 17:44:55 +0100

    added css style

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,7 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.Gioco dei dadi
+// stampa un messaggio appropriato sull’esito del controllo.
+// Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
@@ -10,6 +11,8 @@ const mailList = ["pincopanco@gmail.com" , "boolean-boy@gmail.com" , "afakemail@
 
 const send = document.getElementById("send-email");
 send.addEventListener("click" , checkMail);
+const emailCheck = document.getElementById("emailCheck");
+emailCheck.classList.remove("d-none");
 
 function checkMail()
 {
@@ -17,12 +20,12 @@ function checkMail()
 
     if (mailList.includes(mail))
     {
-        console.log("Mail accettata, benvenuto");
+        emailCheck.innerHTML= `Mail accettata, benvenuto`;
 
     }
     else
     {
-        console.log("Mail rifiutata, accesso bloccato");
+        emailCheck.innerHTML = `Accesso rifiutato`;
     }
 }
 
@@ -38,17 +41,17 @@ function diceGame()
 
     if(playerDice == computerDice)
     {
-        diceGame.innerHTML = `Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Pareggio! :/ </h4>`
+        diceGame.innerHTML = `<div class="card my-5"> Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Pareggio! :/ </h4> </div>`
     }
     else
     {
         if(playerDice > computerDice)
         {
-            diceGame.innerHTML = `Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Hai vinto! :D </h4>`
+            diceGame.innerHTML = `<div class="card my-5"> Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Hai vinto! :D </h4> </div>`
         }
         else
         {
-            diceGame.innerHTML = `Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Hai perso :( </h4>`
+            diceGame.innerHTML = `<div class="card my-5"> Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Hai perso :( </h4> </div>`
         }
     }
 }
