d65e1f5f05e33457086375259ba771a1df9ca2c5
G-Lumia <giorgiolumia@gmail.com>
2023-03-13 17:29:43 +0100

    dice game created

--- js/myscript.js
+++ js/myscript.js
@@ -11,16 +11,44 @@ const mailList = ["pincopanco@gmail.com" , "boolean-boy@gmail.com" , "afakemail@
 const send = document.getElementById("send-email");
 send.addEventListener("click" , checkMail);
 
-function checkMail(){
+function checkMail()
+{
     let mail = document.getElementById("input-email").value.toLowerCase();
 
     if (mailList.includes(mail))
     {
         console.log("Mail accettata, benvenuto");
-        
+
     }
     else
     {
         console.log("Mail rifiutata, accesso bloccato");
+    }
+}
+
+const diceButton = document.getElementById("diceButton");
+diceButton.addEventListener("click" , diceGame);
+
+function diceGame()
+{
+    let playerDice = Math.floor(Math.random() * 6 + 1);
+    let computerDice = Math.floor(Math.random() * 6 + 1);
+    const diceGame = document.getElementById("diceGame");
+    diceGame.classList.remove("d-none");
+
+    if(playerDice == computerDice)
+    {
+        diceGame.innerHTML = `Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Pareggio! :/ </h4>`
+    }
+    else
+    {
+        if(playerDice > computerDice)
+        {
+            diceGame.innerHTML = `Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Hai vinto! :D </h4>`
+        }
+        else
+        {
+            diceGame.innerHTML = `Hai ottenuto ${playerDice} <br> Il computer ha ottenuto ${computerDice} <br> <h4> Hai perso :( </h4>`
+        }
     }
 }
