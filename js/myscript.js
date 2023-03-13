ec316ec7b41d084cec07cae1731543f53a2c2754
G-Lumia <giorgiolumia@gmail.com>
2023-03-13 17:09:13 +0100

    e mail check script reated

--- js/myscript.js
+++ js/myscript.js
@@ -4,3 +4,23 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+
+const mailList = ["pincopanco@gmail.com" , "boolean-boy@gmail.com" , "afakemail@fake.com"];
+
+const send = document.getElementById("send-email");
+send.addEventListener("click" , checkMail);
+
+function checkMail(){
+    let mail = document.getElementById("input-email").value.toLowerCase();
+
+    if (mailList.includes(mail))
+    {
+        console.log("Mail accettata, benvenuto");
+        
+    }
+    else
+    {
+        console.log("Mail rifiutata, accesso bloccato");
+    }
+}
